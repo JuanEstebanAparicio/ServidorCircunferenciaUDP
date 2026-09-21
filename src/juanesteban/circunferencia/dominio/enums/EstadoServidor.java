0963598d413e956745b17057b4cea77ff600d8ec
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2026-09-21 11:07:02 -0500

    nuevos enums

---
+++ src/juanesteban/circunferencia/dominio/enums/EstadoServidor.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juanesteban.circunferencia.dominio.enums;
+
+/**
+ *
+ * @author apari
+ */
+public enum EstadoServidor {
+    DETENIDO,
+    EN_LINEA
+}
---
+++ src/juanesteban/circunferencia/dominio/enums/TipoRespuesta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juanesteban.circunferencia.dominio.enums;
+
+/**
+ *
+ * @author apari
+ */
+public enum TipoRespuesta {
+    CONECTADO,
+    DESCONECTADO,
+    OK_CALCULO,
+    ERROR
+}
